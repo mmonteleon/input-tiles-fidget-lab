206d93cfd3e4ddc6b08891bd2310e02015db9ac3
mmonteleon <mmonteleon@mac.com>
2020-08-19 20:11:28 +0000

    DateInput functioning

--- src/Components/DateInput.js
+++ src/Components/DateInput.js
@@ -2,6 +2,17 @@ import React, {Component} from 'react';
 
 class DateInput extends Component {
   // Add a constructor with state
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: null
+    }
+  };
+
+  // The event will be passed in automatically.
+  handleChange = (event) => {
+    this.setState({"text": event.target.value});
+  }
 
   render() {
     return(
@@ -11,10 +22,10 @@ class DateInput extends Component {
             <div className="card-title">Date</div>
             <p className="card-text">
               <div className="input btn-block">
-                <input type="date" className="textInput" />
+                <input type="date" className="textInput" onChange={this.handleChange}/>
               </div>
               <div className="output">
-                <label for="dateOutput">State: </label>
+                <label for="dateOutput">State: {this.state.text}</label>
                 <span name="dateOutput"></span>
               </div>
             </p>
